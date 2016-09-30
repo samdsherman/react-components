f470bf14ae705edf08db3563518e71ab15aff5fd
sam sherman <samdsherman@gmail.com>
2016-09-30 11:15:05 -0700

    Complete basic requirements

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bold: false
+    };
+  }
+
+  onListItemHover() {
+    this.setState({
+      bold: !this.state.bold
+    });
+  }
+  render() {
+    var style = {
+      fontWeight: this.state.bold ? 'bold' : ''
+    };
+    return <li style={style} onMouseEnter={this.onListItemHover.bind(this)}
+                             onMouseLeave={this.onListItemHover.bind(this)}>
+             {this.props.item}
+           </li>;
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item => <GroceryListItem item={item} />)}
+  </ul>
+);
+ReactDOM.render(<GroceryList items={['cucumbers', 'kale', 'potatoes', 'candy bars']}/>, document.getElementById('app'));
